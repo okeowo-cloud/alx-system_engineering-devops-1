d0451eff14adf2c4fe104925d627c6ea703858b3
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-08-14 16:22:29 +0300

    modify config file using puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+$mytext = "Host 34.75.72.161
+     PasswordAuthentication no
+
+
+     IdentityFile ~/.ssh/holberton"
+
+file {'~/.ssh/.config':
+  ensure  => file,
+  path    => '~/.ssh/.config',
+  content => $mytext,
+}
