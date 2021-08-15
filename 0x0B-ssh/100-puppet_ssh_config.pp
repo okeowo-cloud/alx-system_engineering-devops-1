4ebf3ba9817622878aecfd2ccd188f6eb4c1b681
Amanuel Abiy <amanuelabiy.as@gmail.com>
2021-08-15 15:46:38 +0300

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,14 @@
+define accounts_global::account () {
+
+  account { $name:
+    ensure => present,
+  }
 $mytext = "Host 34.75.72.161
      PasswordAuthentication no
      IdentityFile ~/.ssh/holberton"
 
-file {'~/.ssh/.config':
+file { "/home/${name}/.ssh/config" :
   ensure  => file,
-  path    => '~/.ssh/.config',
+  path    => '/home/${name}/.ssh/config',
   content => $mytext,
 }
