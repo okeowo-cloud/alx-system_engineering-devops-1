3b22b6949485d9addef8bec12fc5e3b9242f0fc0
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-09-13 23:23:27 +0300

    write to csv file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+a script that
+returns information about his/her TODO list progress.
+"""
+import requests
+from sys import argv
+import csv
+
+
+def getText():
+    """ returns information about his/her TODO list progress. """
+    user = (requests.get(
+        'http://jsonplaceholder.typicode.com/users?id={}'.format(
+            argv[1]))).json()
+    res = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv[1]))
+    done_task = []
+    num_all_task = 0
+    num_done_task = 0
+    for todo in res.json():
+        with open('USER_ID.csv', mode='a') as todo_file:
+            todo_writer = csv.writer(
+                todo_file,
+                delimiter=',',
+                quotechar='"',
+                quoting=csv.QUOTE_MINIMAL)
+            todo_writer.writerow([todo.get('userId'), user[0].get('username'),
+                                  todo.get('completed'), todo.get('title')])
+
+
+if __name__ == '__main__':
+    getText()
