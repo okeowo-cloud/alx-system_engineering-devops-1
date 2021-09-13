cf98863f92c1a8094b069d01b32047a410c74cb4
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-09-14 00:27:05 +0300

    all todo lists with user id

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+a script that
+returns information about his/her TODO list progress.
+"""
+import json
+import requests
+
+
+def getText():
+    """ returns information about his/her TODO list progress. """
+    user = (requests.get(
+        'http://jsonplaceholder.typicode.com/users')).json()
+    res = (requests.get(
+        'https://jsonplaceholder.typicode.com/todos')).json()
+
+    to_json = {}
+
+    for userId in range(10):
+        tasks = []
+        username = user[userId].get('username')
+        for todo in res:
+            if todo.get('userId') == userId + 1:
+                dic_todo = {}
+                dic_todo['task'] = todo.get('title')
+                dic_todo['completed'] = todo.get('completed')
+                dic_todo['username'] = username
+                tasks.append(dic_todo)
+        to_json[userId + 1] = tasks
+    filename = "todo_all_employees.json"
+    with open(filename, "w") as f:
+        json.dump(to_json, f, sort_keys=True)
+
+
+if __name__ == '__main__':
+    getText()
