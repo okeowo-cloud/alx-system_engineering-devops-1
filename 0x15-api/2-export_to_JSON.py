af36173983eced3d1f9db75dca6c31d699038349
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-09-13 23:53:23 +0300

    to json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+a script that
+returns information about his/her TODO list progress.
+"""
+import json
+import requests
+from sys import argv
+
+
+def getText():
+    """ returns information about his/her TODO list progress. """
+    user = (requests.get(
+        'http://jsonplaceholder.typicode.com/users?id={}'.format(
+            argv[1]))).json()
+    res = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv[1]))
+    tasks = []
+    to_json = {}
+    userId = user[0].get('id')
+    username = user[0].get('username')
+    for todo in res.json():
+        dic_todo = {}
+        dic_todo['task'] = todo.get('title')
+        dic_todo['completed'] = todo.get('completed')
+        dic_todo['username'] = username
+        tasks.append(dic_todo)
+    to_json[userId] = tasks
+    with open('{}.json'.format(userId), 'w') as todo_file:
+        json.dump(to_json, todo_file)
+
+
+if __name__ == '__main__':
+    getText()
