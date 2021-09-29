854c25a5aa94b8e0ea21ca67f92581a408ab027c
Amanuel Abiy <amanuelabiy.as@gmail.com>
2021-09-29 22:03:35 +0300

    fix wordpress website

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Fix
+exec { '/var/www/html/wp-setting.php':
+  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+}
