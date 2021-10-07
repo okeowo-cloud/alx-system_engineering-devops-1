84c5977195266d86bed9c235032948abc5b40749
Amanuel Abiy <amanuelabiy.as@gmail.com>
2021-10-07 23:30:33 +0300

    fix max number of files opened

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# Fix "accept4() failed (24: Too many open files)" 
+exec {'modify max open files limit setting':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+}
